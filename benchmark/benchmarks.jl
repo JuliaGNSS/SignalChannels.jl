fd0d73928effc9647e2d9bb9ea9773a8c542da57
Soeren Schoenbrod <soeren.schoenbrod@juliahub.com>
2025-12-10 22:24:33 +0100

    perf: add benchmark for signal channel
    Add benchmark for signal channel

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,5 +4,6 @@ using SignalChannels
 const SUITE = BenchmarkGroup()
 
 # Include individual benchmark files
+include("channel_benchmarks.jl")
 include("rechunk_benchmarks.jl")
 include("tee_benchmarks.jl")
---
+++ benchmark/channel_benchmarks.jl
@@ -0,0 +1,49 @@
+using BenchmarkTools
+using SignalChannels
+
+# Number of buffers to push through the channel per benchmark iteration
+const CHANNEL_NUM_BUFFERS = 10_000
+const CHANNEL_NUM_SAMPLES = 2048
+
+# Setup: create producer/consumer pipeline ready to run
+function setup_channel_benchmark(num_samples::Int, buffer_size::Int)
+    ch = SignalChannel{ComplexF32}(num_samples, 1, buffer_size)
+    # Pre-allocate data once
+    data = rand(ComplexF32, num_samples, 1)
+    return (ch, data)
+end
+
+# Benchmark: push data through the channel and drain output
+function run_channel_benchmark!(ch::SignalChannel{ComplexF32}, data::Matrix{ComplexF32}, num_items::Int)
+    # Producer task
+    producer = Threads.@spawn begin
+        for _ in 1:num_items
+            put!(ch, data)
+        end
+        close(ch)
+    end
+
+    # Consumer task
+    consumer = Threads.@spawn begin
+        for _ in ch
+            # discard
+        end
+    end
+
+    wait(producer)
+    wait(consumer)
+    return nothing
+end
+
+# Channel buffer sizes to test
+const CHANNEL_BUFFER_SIZES = [1, 4, 16, 64, 256, 1024]
+
+SUITE["channel"] = BenchmarkGroup()
+
+for buf_size in CHANNEL_BUFFER_SIZES
+    SUITE["channel"]["buffer=$buf_size"] = @benchmarkable(
+        run_channel_benchmark!(ch, data, CHANNEL_NUM_BUFFERS),
+        setup = ((ch, data) = setup_channel_benchmark(CHANNEL_NUM_SAMPLES, $buf_size)),
+        evals = 1
+    )
+end
